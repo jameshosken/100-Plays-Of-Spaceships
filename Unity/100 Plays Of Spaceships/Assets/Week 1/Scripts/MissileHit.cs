6154df82e010b228983626c611f4bb7c4ca652c3
James Hosken <jameswfhosken@gmail.com>
2019-06-08 12:19:07 -0400

    Add day 08

---
+++ Unity/100 Plays Of Spaceships/Assets/Week 1/Scripts/MissileHit.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissileHit : MonoBehaviour
+{
+    [SerializeField] GameObject explosion;
+    [SerializeField] float damage = 101;
+    GameObject player;
+
+    // Start is called before the first frame update
+
+
+    private void Start()
+    {
+        player = GameObject.Find("Player");
+        Physics.IgnoreCollision(player.GetComponentInChildren<Collider>(), GetComponent<Collider>());
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GameObject other = collision.collider.gameObject;
+
+        if(other.tag == "Shootable")
+        {
+            if (other.GetComponent<Health>())
+            {
+                other.GetComponent<Health>().LoseHealth(damage);
+            }
+        }
+
+        GameObject expl = Instantiate(explosion, transform.position, Quaternion.identity);
+        GameObject.Destroy(gameObject);
+    }
+}
---
+++ Unity/100 Plays Of Spaceships/Assets/Week 1/Scripts/MissileLauncher.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissileLauncher : MonoBehaviour
+{
+
+    [SerializeField] GameObject missile;
+    [SerializeField] float maxRate;
+    [SerializeField] float initialVelocity;
+
+    [SerializeField] Transform missileLauncherLocation;
+
+    TargettingHandler targetting;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetting = GetComponent<TargettingHandler>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            FireMissile();
+        }
+    }
+
+    private void FireMissile()
+    {
+        Transform target = targetting.GetTarget();
+
+        GameObject clone = Instantiate(missile);
+        clone.transform.position = missileLauncherLocation.position;
+        clone.transform.rotation = missileLauncherLocation.rotation;
+        clone.GetComponent<Rigidbody>().velocity = GetComponent<Rigidbody>().velocity;
+        clone.GetComponent<Rigidbody>().AddForce(transform.forward * initialVelocity, ForceMode.Impulse);
+
+        if (target)
+        {
+            clone.GetComponent<SeekTarget>().SetTarget(target);
+        }
+    }
+}
