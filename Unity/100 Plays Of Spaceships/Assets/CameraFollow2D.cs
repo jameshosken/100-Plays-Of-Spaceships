d815341332ff21be43859a25610e8e75c79ca8ad
James Hosken <jameswfhosken@gmail.com>
2019-06-20 15:36:41 -0400

    Add Day 20

---
+++ Unity/100 Plays Of Spaceships/Assets/CameraFollow2D.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow2D : MonoBehaviour
+{
+    //Place on camera rig parent. Start parent at location of target.
+
+    [SerializeField] Transform target;
+    [SerializeField] float followSpeed = .1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+        transform.position = Vector3.Lerp(transform.position, target.position, followSpeed);
+    }
+}
---
+++ Unity/100 Plays Of Spaceships/Assets/LanderControl2D.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LanderControl2D : MonoBehaviour
+{
+    public enum State { Alive, Dead }
+
+    [SerializeField] private float thrust = 10f;
+    [SerializeField] private float turnSpeed = 10f;
+    [SerializeField] float deathExplosionForce = 100f;
+    [SerializeField] GameObject deathFX;
+    private State state;
+    private Rigidbody body;
+
+    ParticleSystem particles;
+    ParticleSystem.EmissionModule emission;
+    Light light;
+
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        light = GetComponentInChildren<Light>();
+        particles = GetComponentInChildren<ParticleSystem>();
+        emission = particles.emission;
+        state = State.Alive;
+
+        body = GetComponent<Rigidbody>();
+
+        SetConstraints(true);
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        if (state == State.Alive)
+        {
+            HandleInput();
+        }
+    }
+
+    private void HandleInput()
+    {
+        float turn = Input.GetAxis("Horizontal") * -1f; // Invert controls
+        Vector3 turnVector = new Vector3(0, 0, turn) * turnSpeed;
+
+        body.AddRelativeTorque(turnVector);
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if(emission.enabled  != true)
+            {
+                light.enabled = true;
+                emission.enabled = true;
+            }
+            body.AddRelativeForce(Vector3.up * thrust);
+        }
+        else
+        {
+            if (emission.enabled)
+            {
+                light.enabled = false;
+                emission.enabled = false;
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(state == State.Dead)
+        {
+            return;
+        }
+
+
+        Collider collider = collision.collider;
+
+        if (collider.gameObject.layer == LayerMask.NameToLayer("Shootable"))
+        {
+
+            OnDeath(collision.contacts[0].point);
+        }
+    }
+
+    private void OnDeath(Vector3 point)
+    {
+
+        light.enabled = false;
+        emission.enabled = false;
+
+        SetConstraints(false);
+        body.AddExplosionForce(deathExplosionForce, point, 10);
+        state = State.Dead;
+
+        Instantiate(deathFX, point, Quaternion.identity);
+        Invoke("ReloadLevel", 2f);
+        
+    }
+
+    void ReloadLevel()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+
+    private void SetConstraints(bool status)
+    {
+        if (status)
+        {
+            body.constraints = RigidbodyConstraints.FreezePositionZ;
+            body.constraints = RigidbodyConstraints.FreezeRotationX;
+            body.constraints = RigidbodyConstraints.FreezeRotationY;
+        }
+        else
+        {
+            body.constraints = RigidbodyConstraints.None;
+        }
+
+    }
+}
